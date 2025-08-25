84dfdb32889f9a97035c9b7248852dd6b710d0cb
sarthak <sarthakx67@gmail.com>
2025-08-25 11:24:46 +0530

    Day-1

---
+++ Day_1/Power_Calculation.java
@@ -0,0 +1,11 @@
+//Power Calculation
+import java.util.*;
+public class Power_Calculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double base = sc.nextInt();
+        double exponent = sc.nextInt();
+        double ans = Math.pow(base, exponent);
+        System.out.println(ans);
+    }
+}
